762d313cc6fe7eda0684546ab26a407801d6065d
Adil <adilmukhtar327@gmail.com>
2026-05-05 22:22:34 +0500

    Base Project, demo mode only

---
+++ Account.java
@@ -0,0 +1,35 @@
+package BankService;
+
+import java.math.BigDecimal;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Account {
+    
+    private final long id;
+    private BigDecimal balance;
+    private final ReentrantLock lock;
+
+    public Account(long id, BigDecimal balance) {
+        this.id = id;
+        this.balance = balance;
+        this.lock = new ReentrantLock();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public ReentrantLock getLock() {
+        return lock;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+
+}
---
+++ InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package BankService;
+
+public class InsufficientFundsException extends RuntimeException {
+    
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
